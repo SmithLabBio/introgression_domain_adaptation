96cc062ffcc8b293dd7923aab88c1e9e1a66c966
Kerry Cobb <cobbkerry@gmail.com>
2024-02-02 18:53:35 -0600

    Moved training loop to it's own file

--- cnn.py
+++ cnn.py
@@ -6,13 +6,13 @@ import torch
 from torch import nn
 from torch import optim
 from torch.utils.data import DataLoader, random_split
-from torchvision import datasets
-from torchvision.transforms import ToTensor
 from torch.nn.functional import relu, sigmoid
 import fire
-from torchinfo import summary
-from torchviz import make_dot
+# from torchinfo import summary
+# from torchviz import make_dot
 import pandas as pd
+import yaml
+from pydantic import BaseModel
 
 class Block(nn.Module):
     def __init__(self, iteration: int, nOutputs: int, nFeatures: int, nFullyConnected: int):
@@ -56,6 +56,8 @@ class CNN(nn.Module):
         """
         super().__init__()
         assert nFeatures >= nFullyConnected
+        self.config = dict(nBlocks=nBlocks, nFeatures=nFeatures, 
+                nOutputs=nOutputs, nFullyConnected=nFullyConnected)
         self.nFeatures = nFeatures
         self.nOutputs = nOutputs
         self.nFullyConnected = nFullyConnected
@@ -105,75 +107,12 @@ class Dataset(torch.utils.data.Dataset):
         y = self.migrationRates[index] 
         return x, y 
 
-class Trainer():
-    def __init__(self, model, lossFunction, optimizer):
-        self.model = model
-        self.lossFunction = lossFunction
-        self.optimizer = optimizer
-    
-    def trainingStep(self, x, y):
-        model.train()
-        yhat = self.model(x)
-        loss = self.lossFunction(yhat, y.unsqueeze(1))
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
-        return loss.item()
-    
-    def validationStep(self, x, y):
-        yhat = self.model(x)
-        loss = self.lossFunction(yhat, y.unsqueeze(1))
-        return loss.item()
-
-
-batchSize = 64 
-epochs = 10 
-dataset = Dataset("scenario-1.npz", 500)
-nTest = int(len(dataset) * 0.15)
-nVal = int(len(dataset) * 0.15)
-nTrain = len(dataset) - nTest - nVal 
-trainSet, testSet, valSet = random_split(dataset, [nTrain, nTest, nVal])
-trainLoader = DataLoader(trainSet, batch_size=batchSize, shuffle=True) 
-testLoader = DataLoader(testSet, batch_size=batchSize, shuffle=True) 
-validationLoader = DataLoader(valSet, batch_size=batchSize, shuffle=True) 
-
-model = CNN(nBlocks=6, nFeatures=10, nOutputs=1, nFullyConnected=10)
-lossFunction = nn.MSELoss()
-optimizer = optim.Adam(model.parameters())
-trainer = Trainer(model, lossFunction, optimizer)
 
 ## Show details of model
 # testResult = model(torch.randint(0, 4, (2, 51, 400)).float())
 # g = make_dot(result.mean(), params=dict(model.named_parameters()))
 # g.view("cnn")
 # # print(summary(model, input_size=(1, 51, 400))) # Batch size, Rows, Cols. Channel dimension is added in forward function
-
-trainingLossList = []
-# trainingAccuracy = [] # TODO: Compute accuracy
-validationLossList = []
-# validationAccuracy = []
-
-for epoch in range(epochs):
-    cumulativeTrainingLoss = 0.0 
-    for x, y in trainLoader:
-        loss = trainer.trainingStep(x, y)
-        cumulativeTrainingLoss += loss # TODO: Do I need to account for last batch size being smaller? 
-    trainingLoss = cumulativeTrainingLoss / len(trainLoader)  
-    trainingLossList.append(trainingLossList)
-
-    cumulativeValidationLoss = 0.0
-    with torch.no_grad():
-        for x, y in validationLoader:
-            loss = trainer.validationStep(x, y)
-            cumulativeValidationLoss += loss
-    validationLoss = cumulativeValidationLoss / len(validationLoader)
-    validationLossList.append(validationLoss)
-
-    print(f"Epoch {epoch + 1}: training loss = {trainingLoss} -- validation loss = {validationLoss}")
-
-torch.save(model.state_dict(), "scenario-1.pt")
-df = pd.DataFrame.from_dict(dict(trainingLoss=trainingLossList, validationLossList=validationLossList))
-df.to_csv("scenario-1-loss.csv", index=False)
 
 
 # def run():
