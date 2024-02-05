de4a1b5b59a4c96577e386f847def272678b9804
Kerry Cobb <cobbkerry@gmail.com>
2024-02-05 09:54:43 -0600

    Modified test.py to be runnable from command line

--- test.py
+++ test.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
 
 import pandas as pd
-import torch
 import fire
-import cnn
+from cnn import CNN
+from data import Dataset
+import os.path as path
+import torch
+from torch.utils.data import DataLoader
 
 
-def run(statePath, dataPath):
-    data = cnn.Dataset(path=dataPath, nSnps=500)
-    loader = cnn.DataLoader(data, batch_size=64, shuffle=False)
-    config, state = torch.load(statePath)
-    model = cnn.CNN(**config) 
+def run(statePath, dataPath, outPath=""):
+    if not outPath:
+        outPath = f"{path.splitext(dataPath)[0]}-predicted.csv"
+    else:
+        if not path.splitext(outPath)[1] == ".csv":
+            quit(f"Aborted: outPath should have extension \".csv\"")
+    if path.exists(outPath): 
+        quit(f"Aborted: {outPath} already exists")
+    data = Dataset(path=dataPath, nSnps=500)
+    loader = DataLoader(data, batch_size=64, shuffle=False)
+    config, state, _ = torch.load(statePath)
+    model = CNN(**config) 
     model.load_state_dict(state)
     model.eval()
     predicted = []
@@ -20,10 +30,8 @@ def run(statePath, dataPath):
             yhat = model(x)
             predicted.extend(yhat.squeeze(1).tolist())
             target.extend(y.tolist())
-
     df = pd.DataFrame.from_dict(dict(predicted=predicted, target=target))
-    df.to_csv("scenario-1-predicted.csv", index=False)
-
+    df.to_csv(outPath, index=False)
 
 if __name__ == "__main__":
     fire.Fire(run)
