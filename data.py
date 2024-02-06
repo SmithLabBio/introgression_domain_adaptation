b9f7a124ed41b5305fa8cd8b7bb8be4fbd285b66
Kerry Cobb <cobbkerry@gmail.com>
2024-02-06 14:19:02 -0600

    Fix backwards character matrix slice

--- data.py
+++ data.py
@@ -13,6 +13,7 @@ class Dataset(torch.utils.data.Dataset):
         data = np.load(path, allow_pickle=True)
         self.nSnps = nSnps
         self.config = pickle.loads(data["config"])
+        self.summaryStates = data["summaryStats"]
         self.nDatasets = self.config["nDatasets"]
         self.positions = data["positions"]
         self.charMatrices = data["charMatrices"]
@@ -22,7 +23,7 @@ class Dataset(torch.utils.data.Dataset):
         return self.nDatasets 
 
     def __getitem__(self, index):
-        charMatrix = torch.from_numpy(self.charMatrices[index][:self.nSnps, :]) 
+        charMatrix = torch.from_numpy(self.charMatrices[index][:, :self.nSnps]) 
         positions = torch.from_numpy(self.positions[index][:self.nSnps])
         distances = positionsToDistances(positions).unsqueeze(0)
         x = torch.cat((distances, charMatrix), 0).float() 
