c98a602b0a0c5f5a123dc7dfc4a20b5ee998dd7a
kerrycobb <cobbkerry@gmail.com>
2024-03-21 13:39:12 -0500

    Change some file paths

--- testConv1d_keras.py
+++ testConv1d_keras.py
@@ -4,6 +4,6 @@ from keras import models
 from src.data.kerasSecondaryContactDataset import Dataset
 from src.kerasPredict import predict
 
-test = Dataset("secondaryContact1/secondaryContact1-test-100.json", 400, transpose=True)
+test = Dataset("secondaryContact1/secondaryContact1-test-500.json", 500, transpose=True)
 model = models.load_model("secondaryContact1/keras_conv1d_model", compile=False)
 print(predict(model, test))
