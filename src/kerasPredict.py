5eadf6c31ea4a09ee9449e8143f48c9e89544a42
kerrycobb <cobbkerry@gmail.com>
2024-03-27 10:58:00 -0500

    Fix testing to work with sigmoid output

--- src/kerasPredict.py
+++ src/kerasPredict.py
@@ -2,6 +2,7 @@ from sklearn.metrics import confusion_matrix
 import numpy as np
 
 def predict(model, dataset):
-    p = np.argmax(model.predict(dataset.snps), axis=1)
-    r = confusion_matrix(np.argmax(dataset.migrationStates, axis=1), p)
+    prob = model.predict(dataset.snps)
+    pred = np.where(prob > 0.5, 1, 0)
+    r = confusion_matrix(dataset.migrationStates, pred)
     return r
