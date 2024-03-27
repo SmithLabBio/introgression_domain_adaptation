8154433c803b629a2a436da780e287d582a7952f
kerrycobb <cobbkerry@gmail.com>
2024-03-27 10:57:07 -0500

    Fix testing to work with sigmoid output

--- testConv1d_keras.py
+++ testConv1d_keras.py
@@ -1,9 +1,15 @@
 from tensorflow import keras
 from keras import models
+import numpy as np
 
 from src.data.kerasSecondaryContactDataset import Dataset
 from src.kerasPredict import predict
 
-test = Dataset("secondaryContact1/secondaryContact1-test-100.json", 100, transpose=True)
-model = models.load_model("secondaryContact1/keras_conv1d_model", compile=False)
-print(predict(model, test))
+test = Dataset("secondaryContact1/secondaryContact1-test-100.json", 500, transpose=True)
+model = models.load_model("secondaryContact1/keras_conv1d_model")
+# print(predict(model, test))
+
+p = model.predict(test.snps)
+print(np.where(p > 0.5, 1, 0))
+    # p = np.argmax(model.predict(dataset.snps), axis=1)
+    # r = confusion_matrix(np.argmax(dataset.migrationStates, axis=1), p)
