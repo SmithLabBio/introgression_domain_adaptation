8aa216fbaf2ee2f4b5167f4c74e7084601346bcc
kerrycobb <cobbkerry@gmail.com>
2024-03-28 16:12:27 -0500

    Remove reduntant test files

--- conv1d_cdan_test.py
+++
@@ -1,9 +0,0 @@
-from tensorflow import keras
-from keras import models
-
-from src.data.kerasSecondaryContactDataset import Dataset
-from kerasPredictSigmoid import predict
-
-test = Dataset("ghost1/ghost1-test-100.json", 100, transpose=True)
-model = models.load_model("ghost1/conv1d_cdan_model", compile=False)
-print(predict(model, test))
