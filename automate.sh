2b23c1c7990b757d5496a4f50f13f12acac27938
kerrycobb <cobbkerry@gmail.com>
2024-04-12 13:13:09 -0500

    Add some commands to simulate datasets

--- automate.sh
+++ automate.sh
@@ -1,18 +1,21 @@
 sub -n sim-secondary -m 8 -w "./src/data/secondaryContact.py secondaryContact1/secondaryContact1.yaml secondaryContact1/secondaryContact1-1000 1000 --force"
 sub -n sim-secondary-test -m 8 -w "./src/data/secondaryContact.py secondaryContact1/secondaryContact1.yaml secondaryContact1/secondaryContact1-test-100 100 --force"
 sub -n sim-secondary-val -m 8 -w "./src/data/secondaryContact.py secondaryContact1/secondaryContact1.yaml secondaryContact1/secondaryContact1-val-100 100 --force"
+sub -n sim-ghost -m 8 -w "./src/data/ghostSecondaryContact.py ghost1/ghost1.yaml ghost1/ghost1-1000 1000 --force"
 sub -n sim-ghost -m 8 -w "./src/data/ghostSecondaryContact.py ghost1/ghost1.yaml ghost1/ghost1-100 100 --force"
 sub -n sim-ghost-test -m 8 -w "./src/data/ghostSecondaryContact.py ghost1/ghost1.yaml ghost1/ghost1-test-100 100 --force"
 
 sub -n sim-secondary -m 8 -w "./src/data/secondaryContact.py secondaryContact1/secondaryContact1.yaml secondaryContact1/secondaryContact1-5000 5000 --force"
 sub -n sim-secondary-test -m 8 -w "./src/data/secondaryContact.py secondaryContact1/secondaryContact1.yaml secondaryContact1/secondaryContact1-test-500 500 --force"
 sub -n sim-secondary-val -m 8 -w "./src/data/secondaryContact.py secondaryContact1/secondaryContact1.yaml secondaryContact1/secondaryContact1-val-500 500 --force"
+sub -n sim-ghost -m 8 -w "./src/data/ghostSecondaryContact.py ghost1/ghost1.yaml ghost1/ghost1-5000 5000 --force"
 sub -n sim-ghost -m 8 -w "./src/data/ghostSecondaryContact.py ghost1/ghost1.yaml ghost1/ghost1-500 500 --force"
 sub -n sim-ghost-test -m 8 -w "./src/data/ghostSecondaryContact.py ghost1/ghost1.yaml ghost1/ghost1-test-500 500 --force"
 
 sub -n sim-secondary -m 8 -w "./src/data/secondaryContact.py secondaryContact2/secondaryContact2.yaml secondaryContact2/secondaryContact2-1000 1000 --force"
 sub -n sim-secondary-test -m 8 -w "./src/data/secondaryContact.py secondaryContact2/secondaryContact2.yaml secondaryContact2/secondaryContact2-test-100 100 --force"
 sub -n sim-secondary-val -m 8 -w "./src/data/secondaryContact.py secondaryContact2/secondaryContact2.yaml secondaryContact2/secondaryContact2-val-100 100 --force"
+sub -n sim-ghost -m 8 -w "./src/data/ghostSecondaryContact.py ghost2/ghost2.yaml ghost2/ghost2-1000 1000 --force"
 sub -n sim-ghost -m 8 -w "./src/data/ghostSecondaryContact.py ghost2/ghost2.yaml ghost2/ghost2-100 100 --force"
 sub -n sim-ghost-test -m 8 -w "./src/data/ghostSecondaryContact.py ghost2/ghost2.yaml ghost2/ghost2-test-100 100 --force"
 
